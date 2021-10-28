a6cdb50ba7ac8e036cc8220f992c38b45073f197
Vlad Filiucov <filiucov@mail.ru>
2021-10-28 23:30:10 +0100

    initial

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  name       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Post < ApplicationRecord
+end
