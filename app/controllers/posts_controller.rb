faddf9b4e4310daf7e2f52d0d077fddde0f15be4
Vlad Filiucov <filiucov@mail.ru>
2021-10-29 11:26:58 +0100

    finish infinite scroll

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,13 @@ class PostsController < ApplicationController
   # GET /posts
   def index
     @pagy, @posts = pagy(Post.all)
+
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: { entries: render_to_string(partial: "posts", formats: [:html]), pagination: view_context.pagy_nav(@pagy) }
+      }
+    end
   end
 
   # GET /posts/1
--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -1,9 +1,32 @@
 import { Controller } from "stimulus"
+import Rails from '@rails/ujs';
 
 export default class extends Controller {
   static targets = ["entries", "pagination"]
 
   scroll() {
-    console.log('Scrolling');
+    const body = document.body;
+    const html = document.documentElement;
+    const url = this.paginationTarget.querySelector("a[rel='next']")?.href
+
+    const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
+
+    if (window.pageYOffset >= height - window.innerHeight) {
+      if (url) this.loadMore(url);
+    }
+  }
+
+  loadMore(url) {
+    Rails.ajax({
+      type: 'GET',
+      url,
+      dataType: 'json',
+      success: (data) => {
+        // console.log('Got more data', data);
+        // console.log('what', this.entriesTarget);
+        this.entriesTarget.insertAdjacentHTML('beforeend', data.entries);
+        this.paginationTarget.innerHTML = data.pagination;
+      }
+    });
   }
 }
